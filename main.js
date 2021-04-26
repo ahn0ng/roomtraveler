314b520c381a935b4a229f0aa78c3823a4793c24
ahn0ng <ahn93sk@gmail.com>
2021-04-27 00:38:07 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,14 @@
+$(function(){ 
+    $("#button").click(function(){
+        $("#modal_wrap").fadeIn();
+        $("#modal").fadeIn();
+        // $("body").css("overflow","hidden");
+    });
+
+    $("#modal").find("span").click(function(){
+        $("#modal_wrap").fadeOut();
+        $("#modal").fadeOut();
+        // $("body").css("overflow","auto");
+    });
+
+});
